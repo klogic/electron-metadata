f7e9435bdba470b17b07b4fdae710c33e0a4c300
narongsak keawmanee <klogic@hotmail.co.th>
2020-02-11 19:45:04 +0700

    show pre-text with ipcRenderer and ipcMain

--- main.js
+++ main.js
@@ -1,9 +1,34 @@
-const { app, BrowserWindow } = require("electron");
-const path = require("path");
+const { app, ipcMain, BrowserWindow } = require("electron");
+require("electron-reload")(__dirname);
 
+const path = require("path");
+const util = require("util");
+const fs = require("fs");
+
+const stat = util.promisify(fs.stat);
 let mainWindow;
 app.on("ready", () => {
   const htmlPath = path.join("src", "index.html");
-  mainWindow = new BrowserWindow();
+  mainWindow = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
   mainWindow.loadFile(htmlPath);
+});
+
+ipcMain.on("files", async (event, filesArr) => {
+  try {
+    console.log(filesArr);
+    const data = await Promise.all(
+      filesArr.map(async ({ name, pathName }) => ({
+        ...(await stat(pathName)),
+        name,
+        pathName
+      }))
+    );
+    mainWindow.webContents.send("metadata", data);
+  } catch (error) {
+    mainWindow.webContents.send("metadata:error", error);
+  }
 });
--- src/renderer.js
+++ src/renderer.js
@@ -1,7 +1,21 @@
+const { ipcRenderer } = require("electron");
 const submitListener = document
   .querySelector("form")
   .addEventListener("submit", event => {
     event.preventDefault();
-    const files = document.getElementById("filePicker").files;
-    console.log(files);
+    const files = [...document.getElementById("filePicker").files];
+    const fileFormatted = files.map(({ name, path: pathName }) => ({
+      name,
+      pathName
+    }));
+    ipcRenderer.send("files", fileFormatted);
   });
+
+ipcRenderer.on("metadata", (event, metadata) => {
+  const pre = document.getElementById("data");
+  pre.innerText = JSON.stringify(metadata, null, 2);
+});
+
+ipcRenderer.on("metadata:error", (event, error) => {
+  console.error(error);
+});
